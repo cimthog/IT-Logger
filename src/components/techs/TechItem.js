b969e9ce113db9e637dc9a2cc667907e56f9b8b4
cimthog <danieldamlare@gmail.com>
2019-12-18 23:23:16 +0100

    finished redux for managing tech state

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { deleteTech } from "../../actions/techActions";
 import PropTypes from "prop-types";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(id);
+    M.toast({ html: "Technician Deleted" });
+  };
+
   return (
     <li className="collection-item">
       <div>
-        {tech.firstName} {tech.lastName}
+        {firstName} {lastName}
         <a href="#!" className="secondary-content">
-          <i className="material-icons grey-text">delete</i>
+          <i className="material-icons grey-text" onClick={onDelete}>
+            delete
+          </i>
         </a>
       </div>
     </li>
@@ -15,7 +25,8 @@ const TechItem = ({ tech }) => {
 };
 
 TechItem.propTypes = {
-  tech: PropTypes.object.isRequired
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
